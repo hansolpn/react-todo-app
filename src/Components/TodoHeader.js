86a8b7f293a14dac75e935a2bfd7cdfee7a93da9
hansol na <142369934+hansolpn@users.noreply.github.com>
2023-11-23 16:37:37 +0900

    231123 todo
    # 3. 할일 추가 form 이벤트 처리
    # 4. 할 일 추가 기능 완료
    # 5. 할 일 삭제 기능 완료
    # 6. 할 일 체크 기능, 시간 표시 완료

--- src/Components/TodoHeader.js
+++ src/Components/TodoHeader.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './scss/TodoHeader.scss';
 
-const TodoHeader = () => {
+const TodoHeader = ({ count }) => {
+  const today = new Date();
+
+  const dateString = today.toLocaleDateString('ko-kr', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const dayName = today.toLocaleDateString('ko-kr', { weekday: 'long' });
   return (
     <header>
-      <h1>2023년 11월 23일</h1>
-      <div className='day'>목요일</div>
-      <div className='tasks-left'>할 일 x개 남음</div>
+      <h1>{dateString}</h1>
+      <div className='day'>{dayName}</div>
+      <div className='tasks-left'>할 일 {count()}개 남음</div>
     </header>
   );
 };
